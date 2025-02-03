96468d32efdab47d8f1a3b67189a6f6d6edef009
Ruben Socha <ruben@socha.se>
2025-02-03 16:38:27 +0100

    add: ci_server.py with code skeleton and __init__.py

---
+++ src/ci_server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import subprocess
+
+
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    pass
+
+def clone_repo():
+    pass
+
+def build_project():
+    pass
+
+def run_tests():
+    pass
+
+def update_github_status():
+    pass
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
