fd25f0601e200e04f1be9ae6c75ffd5a288450f6
BitGatito <usmanbinmujeeb@gmail.com>
2025-02-13 01:32:03 +0100

    fix: reverted mock values

--- tests/test_ci_server.py
+++ tests/test_ci_server.py
@@ -63,8 +63,8 @@ def test_handle_webhook(
     assert response.status_code == 200
 
 
-# @patch("ci_server.get_db")
-# @patch("ci_server.insert_build")
+@patch("ci_server.get_db")
+@patch("ci_server.insert_build")
 @patch("ci_server.clone_repo")
 @patch("ci_server.build_project")
 @patch("ci_server.run_tests")
@@ -74,15 +74,15 @@ def test_process_request(
     mock_run_tests,
     mock_build,
     mock_clone_repo,
-    # mock_insert_build,
-    # mock_get_db,
+    mock_insert_build,
+    mock_get_db,
 ):
     # Passing commit
     mock_clone_repo.return_value = (True, "/repo/path")
     mock_build.return_value = True
     mock_run_tests.return_value = (True, "")
-    # mock_get_db.return_value = None
-    # mock_insert_build.return_value = 1
+    mock_get_db.return_value = None
+    mock_insert_build.return_value = 1
 
     payload = {
         "repository": {
